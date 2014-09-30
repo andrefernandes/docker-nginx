f90d6b649270d620651da5bb062a50b77d7c4b73
andre <goroh@hotmail.com>
2014-09-30 08:09:10 -0300

    quickweb

--- quickweb.sh
+++ quickweb.sh
@@ -1,5 +1,7 @@
+echo "server { listen 8000; location / { autoindex on;}}" > /tmp/dirsite.conf
 docker run -t -i --rm \
-   -p 80:80 \
+   -p 8000:8000 \
    -v $PWD:/usr/share/nginx/html \
+   -v /tmp/dirsite.conf:/etc/nginx/sites-enabled/dirsite.conf \
    andrefernandes/docker-nginx:latest ${@}
 
